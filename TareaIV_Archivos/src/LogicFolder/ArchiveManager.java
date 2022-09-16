aaf3f0553bf9aa6140370ab9df02cd370b26bc04
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-16 10:23:02 -0600

    Initial creation of folders and archives

---
+++ TareaIV_Archivos/src/LogicFolder/ArchiveManager.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LogicFolder;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author pamelamurillo
+ */
+public class ArchiveManager {
+    File usersFile;
+    File carsFile;
+    
+    public void createFileUsers(){
+        usersFile = new File("users.txt");
+    }
+    
+    public void createFileCars(){
+        usersFile = new File("users.txt");
+    }
+    
+    public void writeInFile(String fileString, String textToWrite) { //This can get "users" or "books", and the text you wanna write in it
+        File nameFile = carsFile;      //The automatic file creaded is the booksFile
+        if ("users".equals(fileString)) {    //Unless it is specified the contrary
+            nameFile = usersFile;
+        }
+        
+        try {
+            
+            FileWriter writer;
+            writer = new FileWriter(nameFile, true);
+
+            BufferedWriter bufferWriter = new BufferedWriter(writer);
+
+            bufferWriter.write(textToWrite);        //writes the text
+            bufferWriter.newLine();                 //Goes to the next line
+            bufferWriter.close();                   //closes the file
+
+        } catch (IOException ex) {
+            Logger.getLogger(ArchiveManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public ArrayList readInFile(String fileString){ //This can get "users" or "books"
+        File nameFile = carsFile;      //The automatic file creaded is the booksFile
+        if ("users".equals(fileString)) {    //Unless it is specified the contrary
+            nameFile = usersFile;
+        }
+        
+        ArrayList infoFile = new ArrayList();
+        String line;
+        
+        try {
+            FileReader reader = new FileReader(nameFile);
+            BufferedReader bufferReader = new BufferedReader(reader);
+
+            while ( (line = bufferReader.readLine()) != null ) {
+                infoFile.add(line);
+            }
+            
+            bufferReader.close();
+        
+        } catch (IOException ex) {
+            Logger.getLogger(ArchiveManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return infoFile;
+    }
+    
+    public void deleteAFile(String fileString) {
+        File nameFile = carsFile;           //The automatic file creaded is the booksFile
+        if ("users".equals(fileString)) {    //Unless it is specified the contrary
+            nameFile = usersFile;
+        }
+        
+        nameFile.delete();
+    }
+}
--- TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
+++ TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
@@ -4,6 +4,8 @@
  */
 package tareaiv_archivos;
 
+import Interfaces.LoginFrame;
+
 /**
  *
  * @author pamelamurillo
@@ -14,7 +16,8 @@ public class MainStart {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        LoginFrame newWindow = new LoginFrame();
+        newWindow.setVisible(true);
     }
     
 }
