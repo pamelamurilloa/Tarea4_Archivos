8a8c80b5cbcf0c0116187be40b07ed1c403d85b8
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-16 09:53:12 -0600

    Updating new project to github

---
+++ TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tareaiv_archivos;
+
+/**
+ *
+ * @author pamelamurillo
+ */
+public class MainStart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
