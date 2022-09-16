4ee5ee49bc3bde9ca7e3e99754167715d1b92d5e
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-16 10:34:17 -0600

    New object "Car" is now available

---
+++ TareaIV_Archivos/src/Objects/Car.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Objects;
+
+/**
+ *
+ * @author pamelamurillo
+ */
+public class Car {
+    private String brand;
+    private String color;
+    private String numberPlate;
+    private String type;
+    private int maxSpeed;
+    private int price;
+
+    public Car(String brand, String color, String numberPlate, String type, int maxSpeed, int price) {
+        this.brand = brand;
+        this.color = color;
+        this.numberPlate = numberPlate;
+        this.type = type;
+        this.maxSpeed = maxSpeed;
+        this.price = price;
+    }
+
+    
+    
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNumberPlate() {
+        return numberPlate;
+    }
+
+    public void setNumberPlate(String numberPlate) {
+        this.numberPlate = numberPlate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+    
+    
+}
