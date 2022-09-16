b3c18343c13a5b17a27d7755885ebf78c8835c73
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-16 11:24:18 -0600

    Filling files at the start

--- TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
+++ TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
@@ -1,21 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package tareaiv_archivos;
 
 import Interfaces.LoginFrame;
+import LogicFolder.CarManager;
 
-/**
- *
- * @author pamelamurillo
- */
+
 public class MainStart {
 
-    /**
-     * @param args the command line arguments
-     */
+    private static CarManager carManager = new CarManager();
+    
+    
     public static void main(String[] args) {
+        carManager.fillFiles();
         LoginFrame newWindow = new LoginFrame();
         newWindow.setVisible(true);
     }
