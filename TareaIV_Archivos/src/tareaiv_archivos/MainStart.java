5c6728ca478c11192a7b2308db487ee31d4e41c7
Pamela Murillo <pamelamurilloa@gmail.com>
2022-09-19 15:07:21 -0600

    Administrator added to the files

--- TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
+++ TareaIV_Archivos/src/tareaiv_archivos/MainStart.java
@@ -1,16 +1,18 @@
 package tareaiv_archivos;
 
 import Interfaces.LoginFrame;
-import LogicFolder.CarManager;
+import LogicFolder.*;
+import java.io.File;
 
 
 public class MainStart {
 
     private static CarManager carManager = new CarManager();
-    
+    private static ArchiveManager archiveManager = new ArchiveManager();
     
     public static void main(String[] args) {
         carManager.fillFiles();
+        archiveManager.addAdministrator("Admin", "123");
         LoginFrame newWindow = new LoginFrame();
         newWindow.setVisible(true);
     }
